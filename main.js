c522d2f4c31170e806c27f3239357b69e9c79b46
DruxAMB <druxambmarketing@gmail.com>
2023-09-26 07:30:03 -0700

    general commit

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ var b = document.getElementById("female");
 var c = document.getElementById("btn");
 var d = document.getElementById("head");
 var e = document.getElementById("para");
+var f = document.getElementById("rst");
 
 var val = Math.floor(Math.random() * 100);
-function clickMe() {
+clickMe=()=> {
+
+  c.style.display="none";
+  f.style.display="block";
+
   if ((d.innerHTML = val >= 55)) {
     d.innerHTML = val + "%";
     e.innerHTML =a.value +" and "+b.value+" the strength of your love is extreme👫🏽💑😱!!!, You can also try calculating your friends love strength.";
@@ -14,4 +19,8 @@ function clickMe() {
     d.innerHTML = val + "%";
     e.innerHTML =a.value+" and "+b.value+" the strength of your love is a little low 🥀😥💔!!!, You can also try calculating your friends love strength.";
   }
+}
+
+clickRst=()=> {
+  location.reload();
 }
